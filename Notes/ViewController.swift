3457e9b3ac46fe3e915bba4f079e13ce07ec34d3
Pavel Vasilenko (skype userow, telegram @userow81) <userow@gmail.com>
2019-07-04 22:08:37 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Paul Vasilenko on 7/4/19.
+//  Copyright © 2019 Paul Vasilenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
